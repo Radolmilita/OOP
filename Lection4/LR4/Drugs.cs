99995f14e5013ad9b1c7de03c7ec231753c20990
Radolmilita <65241148+Radolmilita@users.noreply.github.com>
2021-10-09 14:20:50 +0300

    Add files via upload

---
+++ Lection4/LR4/Drugs.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR4
+{
+    class Drugs
+    {
+        private string drugsName;
+        private int amount;
+        private string form;
+        private double price;
+
+        public Drugs(int amount, int price) {
+            this.price = price;
+            this.amount = amount;
+        }
+
+        public void Swap(int amount, int price) {
+            int temp = price;
+            price = amount;
+            amount = temp;
+            Console.WriteLine($"Название {drugsName} \nЦена {price} \nКоличество {amount} {Environment.NewLine}");
+        }
+
+        public double Price { 
+            set => price = value;
+            get => price; 
+        }
+        public int Amount {
+            set => amount = value;
+            get => amount;
+        }
+        public void SetName(string drugsName) { this.drugsName = drugsName; }
+        public string GetName() { return drugsName; }
+
+        public void AddNDS(ref double fullPrice) {
+            double a = 0.2;
+            fullPrice = price + (price * a);
+
+            Console.WriteLine($"{Environment.NewLine}Название {drugsName} \nЦена с НДС {fullPrice} {Environment.NewLine}");
+        }
+
+        public void Input(out int sat) {
+            sat = 800;
+            Console.WriteLine($"sat= {sat}");
+        }
+
+        //использование params
+        public void Fun(params int [] sf) {  
+            foreach (int i in sf)
+                Console.WriteLine($"parametr = {i}");
+            Console.WriteLine("\n");
+        }
+
+        public int NamedArguments(int first, int second, int third) {
+            return first + second * third;   
+        }
+
+        public void OptionalArguments(int opt3, int opt1 = 4, int opt2 = 2) {
+            Console.WriteLine($"optional argument f = {opt1}, \noptional argument s= {opt2} \noptional argument t = {opt3}");
+        }
+
+        public void Comparison(int amount, int price) {
+            if (amount > price) {
+                Console.WriteLine("Было в достатке");
+            } else if (amount == price) {
+                Console.WriteLine("Мы где-то зашли не там");
+            }
+            else {
+                Console.WriteLine("Походу дефицит, поднимай ещё цены!");
+            }
+            
+        }
+
+        public override string ToString()
+        {
+            return $"Название {drugsName} \nЦена {price} \nКоличество {amount}";
+        }
+    }
+}
---
+++ Lection4/LR4/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR4
+{
+    class Program
+    {
+        public void Change(Drugs citramon) {
+            citramon.Price = 20;
+            citramon.Amount = 30;
+        }
+        static void Main(string[] args)
+        {
+            Drugs citramon = new Drugs(30, 20);//for reference
+            Program pr = new Program();
+            citramon.SetName("Цитрамон");
+            pr.Change(citramon);
+            Console.WriteLine(citramon.ToString());
+
+            Console.WriteLine("");
+
+            Drugs aspirin = new Drugs(10, 70);//for value
+            int price = 10, amount = 70;
+            aspirin.SetName("Аспирин");
+            aspirin.Swap(price, amount);
+            Console.WriteLine(aspirin.ToString());
+            aspirin.Comparison(price, amount);
+
+            Drugs analgin = new Drugs(30, 80); //for ref
+            analgin.SetName("Анальгин");
+            analgin.Price = 30; 
+            analgin.Amount = 80;
+            double fullPrice = analgin.Price;
+            analgin.AddNDS(ref fullPrice);
+
+
+            Drugs other = new Drugs(1, 1); //for out
+            other.Input(out var sout);
+            Console.WriteLine($"type sat = {sout.GetType()} \nsout= {sout}");
+            int cpy = 9;
+            other.Fun(4, 12, 15, 7);
+            other.Fun(0, 8, 10);
+            other.Fun(cpy, 8, 14, 58);
+            other.Fun();
+            int result, result1;
+            result = other.NamedArguments(1, 6, 7);
+            result1 = other.NamedArguments(first: 3, second: 11, 67);
+            Console.WriteLine($"Результат = {result} Резульат1 = {result1}");
+            Console.WriteLine("\n");
+            other.OptionalArguments(1,2,5);
+            other.OptionalArguments(7, 3, 5);
+            other.OptionalArguments(1);           
+            Console.ReadKey();
+        }
+    }
+}
